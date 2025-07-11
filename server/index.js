84fbbaa154276d2d323c2f03e84cf34e7c6cb993
sach1810 <sach1810@gmail.com>
2025-07-11 05:07:38 -0700

    more deployment updates

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ dotenv.config();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const isProduction = process.env.NODE_ENV === "production";
 
 let server;
